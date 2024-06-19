ea97ac513223c1ea167b5c2a7ffaeb0594eff8df
Zergo62 <s.o.83@mail.ru>
2024-06-19 09:35:55 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+print('Практическая работа по уроку "Организация программ и методы строк."')
+my_string = input('Введите произвольный текст ')
+print('Ваш текст состоит из', len(my_string), 'символов')
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
