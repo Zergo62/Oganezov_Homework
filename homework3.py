d29a14a7e3eceee594c35aa6726f07340b4ccb7a
Zergo62 <s.o.83@mail.ru>
2024-06-19 14:40:34 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+print('Практическая работа по уроку "Динамическая типизация"')
+name = 'Sergo'
+print('Name:', name)
+age = 40
+print('Age:', age)
+age = age + 1
+print ('New age:', age)
+is_student = True
+print('Is Student:', is_student)
