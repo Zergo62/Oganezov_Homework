4b89a301e1f0d557654614f352b0217cd4dd787f
Zergo62 <s.o.83@mail.ru>
2024-07-19 17:04:58 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+# связан с module_4_1.py
+
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    return first / second
---
+++ true_math.py
@@ -0,0 +1,8 @@
+# связан с module_4_1.py
+
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    return first / second
