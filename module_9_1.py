8b4ac5db65ac9ba694d036307dab0ef67a0fd885
Zergo62 <s.o.83@mail.ru>
2024-10-09 16:52:40 +0300

    Add files via upload

--- module_9_1.py
+++ module_9_1.py
@@ -7,10 +7,3 @@ def apply_all_func(int_list, *functions):
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
-
-
-
-
-
-# dict = { "Фрукты": 22, "Пустая": 14, "Корица": 4, "Сыр": 21}
-# dict["Вишня"] = 10  - получаем новый словарь
