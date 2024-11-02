7d34138b12f14ce088250e7d9be848b1623305cb
Zergo62 <s.o.83@mail.ru>
2024-11-02 17:17:41 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,8 +1,8 @@
 # Домашнее задание по теме "Генераторы"
 def all_variants(text):
-    for i in range(len(text)):
-        for r in range(i + 1, len(text) + 1):
-            yield text[i:r]
+    for i in range(1, len(text) + 1):
+        for r in range(len(text) - i + 1):
+            yield text[r:r + i]
 
 a = all_variants("abc")
 for i in a:
