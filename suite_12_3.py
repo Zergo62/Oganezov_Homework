2b67b0f0a49de315f82aad7a11e3f6f739deebac
Zergo62 <s.o.83@mail.ru>
2024-12-02 17:37:51 +0300

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,9 @@
+# Домашнее задание по теме "Систематизация и пропуск тестов"
+import unittest
+import tests_12_3
+
+testST = unittest.TestSuite()
+testST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
+testST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(testST)
