4ed793d3c92198d3608ed38e6c71c7192cfa29fa
Zergo62 <s.o.83@mail.ru>
2025-02-03 16:23:04 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -11,17 +11,32 @@ async def get_all_users() -> dict:
     return users
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: str) -> str:
+async def add_user(username: Annotated[str, Path(min_length=2,
+                                                 max_length=10,
+                                                 description='Enter username',
+                                                 example='UrbanUser')],
+                   age: int = Path(ge=18, le=120, description='Enter age', example=24)) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f'Имя: {username}, возраст: {age}'
     return f'User {current_index} is registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id, username, age) -> str:
+async def update_user(user_id: Annotated[int, Path(ge=1,
+                                                   le=100,
+                                                   description='Enter User ID',
+                                                   example=56)],
+                      username: str = Path(min_length=2,
+                                           max_length=10,
+                                           description='Enter username',
+                                           example='UrbanUser'),
+                      age: int = Path(ge=18, le=120, description='Enter age', example=24)) -> str:
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is updated'
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id) -> str:
+async def delete_user(user_id: Annotated[int, Path(ge=1,
+                                                   le=100,
+                                                   description='Enter User ID',
+                                                   example=56)]) -> str:
     users.pop(user_id)
     return f'User {user_id} was deleted'
